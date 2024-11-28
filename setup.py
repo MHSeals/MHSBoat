04cf536c68d4a74039a723db4dd2d6c9e7557631
1unarzDev <lunarzDev@outlook.com>
2024-11-28 16:45:18 -0600

    Fixed Errors

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = "mhsboat"
+
+setup(
+    name=package_name,
+    version="0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    author="Martin High School",
+    description="MHSeals Boat Scripts",
+    license="GNU GPLv3",
+    tests_require=["pytest"],
+)
